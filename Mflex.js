514b9843312d112841a8c1690def796e43909dac
Daryl Deogracias <daryldeogracias@Daryls-MacBook-Air.local>
2023-07-03 10:47:55 +0800

    initialized git repository new update

--- Mflex.js
+++ Mflex.js
@@ -1,59 +1,94 @@
-// https://www.omdbapi.com/?apikey=7093b65d&s=
-// https://api.themoviedb.org/3/movie/550?api_key=59a2cc2fa5db4670eabfee80c179fa9a
-// https://api.themoviedb.org/3/trending/all/day?api_key=59a2cc2fa5db4670eabfee80c179fa9a
-// https://api.themoviedb.org/3/discover/movie?api_key=59a2cc2fa5db4670eabfee80c179fa9a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
-
-const movieListEl = document.querySelector(".movies");
- 
-
-
-async function main() {
-    const movies = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=59a2cc2fa5db4670eabfee80c179fa9a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate")
-    const moviesData = await movies.json();
-    console.log(moviesData)
-    movieListEl.innerHTML = moviesData.results.map(movie => moviesTrend(movie)).join("");
-       
-}
-
-main()
-function moviesTrend(movie) {
-    return `<div class="movie"> 
-    <figure class="movies__wrapper">
-      <img class="movie__img" src="https://www.themoviedb.org/t/p/w500${movie.poster_path}" alt="">
-    </figure>
-    <div class="movie__description"> 
-     <h4 class="movie__title">
-     ${movie.original_title}
-     </h4>
-     <h4 class="movie__year" class="text-color1">${movie.vote_average}</h4>
-    </div>
-    <div class="movie__description2">
-        <h4 class="movie__statss">
-        ${movie.original_language}
-        </h4>
-        <h4 class="movie__stats"><i class="fa-solid fa-thumbs-up"></i> ${movie.popularity}</h4> 
-    </div>
- </div> `
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(function () {
+    let API_KEY = "api_key=59a2cc2fa5db4670eabfee80c179fa9a";
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const API_URL = `/discover/movie?sort_by=popularity.desc&${API_KEY}`;
+    const SEARCH_URL = `${BASE_URL}/search/movie?${API_KEY}`;
+    const movieListEl = document.querySelector(".movies");
+  
+    const form = document.querySelector(".form");
+    const search = document.querySelector("#search");
+    const fa = document.querySelector(".fa");
+  
+    async function fetchMovies(url) {
+      const response = await fetch(url);
+      const moviesData = await response.json();
+      console.log(moviesData);
+      movieListEl.innerHTML = moviesData.results
+        .map((movie) => moviesTrend(movie))
+        .filter((movieHTML) => movieHTML !== null) // Filter out movies with null value
+        .join("");
+    }
+  
+    const searchIcon = document.querySelector(".fa");
+    searchIcon.addEventListener("click", () => {
+      form.dispatchEvent(new Event("submit"));
+      scrollToSection();
+    });
+  
+    async function main() {
+      await fetchMovies(BASE_URL + API_URL);
+    }
+  
+    main();
+  
+    function moviesTrend(movie) {
+      if (!movie.poster_path) {
+        return null; // Skip movies without a poster image
+      }
+  
+      return `<div class="movie"> 
+        <figure class="movies__wrapper">
+          <img class="movie__img" src="https://www.themoviedb.org/t/p/w500${movie.poster_path}" alt="">
+        </figure>
+        <div class="movie__description"> 
+         <h4 class="movie__title">
+         ${movie.original_title}
+         </h4>
+         <h4 class="movie__year text-color1">${movie.vote_average}</h4>
+        </div>
+        <div class="movie__description2">
+            <h4 class="movie__statss">
+            ${movie.original_language}
+            </h4>
+            <h4 class="movie__stats"><i class="fa-solid fa-thumbs-up"></i> ${movie.popularity}</h4> 
+        </div>
+     </div> `;
+    }
+  
+    // Search
+  
+    const clearInput = () => {
+      search.value = "";
+    };
+  
+    const clearBtn = document.getElementById("clear-btn");
+    clearBtn.addEventListener("click", clearInput);
+  
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+  
+      const searchTerm = search.value;
+  
+      if (searchTerm) {
+        const searchUrl = `${SEARCH_URL}&query=${searchTerm}`;
+        await fetchMovies(searchUrl);
+        clearInput();
+        scrollToSection();
+      }
+    });
+  
+    function scrollToSection() {
+      const mySection = document.querySelector(".Mflexmovie");
+      const sectionOffsetTop = mySection.offsetTop;
+      const scrollToPosition = sectionOffsetTop - 20; // Adjust scroll position as needed
+  
+      window.scrollTo({
+        top: scrollToPosition,
+        behavior: "smooth",
+      });
+    }
+  })();
+  
 
 
  
